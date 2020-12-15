0959e2122ca44f4228d581eb023e4845172b7a89
EmilyNaylor <emmylunaylor@gmail.com>
2020-12-15 13:51:32 -0700

    update

--- NAYLOR_final_project/R/03_ASSET_Htraits_analysis.R
+++ NAYLOR_final_project/R/03_ASSET_Htraits_analysis.R
@@ -9,6 +9,8 @@
 
 ## Load the package ASSET ##
 library("ASSET")
+library(tidyverse)
+library(patchwork)
 
 
 ## Load in the data "Data/SNP_Data_Organized.csv" ##
@@ -53,6 +55,30 @@ write.csv(sum,file="Data/h_trait_analysis.csv")
 ## retrieve the 1 sided subset ##
 sd1 <- sum$Subset.1sided
 
-## select SNPs with 3 or more traits association
+## select SNPs with 3 or more traits association ##
+disease3 <- filter(sd1,Pheno == "CeD,RA,SSc,T1D" | Pheno == "CeD,RA,SSc" |
+         Pheno == "CeD,RA,T1D"|Pheno == "CeD,SSc,T1D" |
+         Pheno == "RA,SSc,T1D")
 
+## Create a table with the SNP information ##
 
+## Create vector with the SNPs ##
+high_SNPS <- disease3$SNP
+
+## filter out the relevant SNPs and data from the original dataset ##
+dat <- df %>% select("region","chr","position_bp","snp","a1","gene")%>%
+  filter(snp %in% high_SNPS)
+
+## keep SNP variable name the same ##
+names(disease3)[names(disease3) == "SNP"] <- "snp"
+
+## join the data together as results##
+results <- full_join(disease3,dat,by="snp")
+
+## Export the results of the SNP analysis ##
+write.csv(results, file = "Results/Tables/SNP_results.csv")
+
+## Create forest plots for each gene ##
+
+## AFF3 ##
+h.forestPlot(res, "rs13415465")
